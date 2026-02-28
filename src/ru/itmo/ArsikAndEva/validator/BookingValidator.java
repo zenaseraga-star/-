4489628e85f5273c4cdec6e9001a634507ffe39b
zenaseraga-star <zenaseraga@gmail.com>
2026-02-28 16:08:20 +0300

    Update BookingValidator.java

--- src/ru/itmo/ArsikAndEva/validator/BookingValidator.java
+++ src/ru/itmo/ArsikAndEva/validator/BookingValidator.java
@@ -1,4 +1,23 @@
 package ru.itmo.ArsikAndEva.validator;
+import model;
+import exception.ValidationException;
 
-public class BookingValidator {
-}
+public class BookingValidator implements Validator<Booking> {
+    @Override
+    public void validate(Booking booking){
+        if(booking == null) {
+            throw  new ValidationException("Бронь не осуществлялась" );
+        }
+        if(booking.getStatus() == null){
+            throw new ValidationException("У брони должен быть статус");
+        }
+        if(booking.getOwnerUsername() == null || booking.getOwnerUsername().isEmpty() || booking.getOwnerUsername().isBlank()){
+            throw new ValidationException("Надо указать свой логин");
+        }
+
+        
+        }
+        
+
+        
+    }
