833298e91ec85a16ab2773d3038789f75fcb9d0c
Aln0270 <arslan.uzenbayev.07@bk.ru>
2026-03-13 16:22:43 +0300

    Update CheckoutValidator.java
    добавил больше проверок

--- src/ru/itmo/ArsikAndEva/validator/CheckoutValidator.java
+++ src/ru/itmo/ArsikAndEva/validator/CheckoutValidator.java
@@ -1,26 +1,28 @@
 package ru.itmo.ArsikAndEva.validator;
-import ru.itmo.ArsikAndEva.model.Instrument;
-import model.Checkout;
+import ru.itmo.ArsikAndEva.model.Checkout;
 import ru.itmo.ArsikAndEva.exception.ValidationException;
 
 public class CheckoutValidator implements Validator<Checkout> {
     @Override
-    puvlic void validate(Checkout checkout){
-        if( checkout.getUsername() == null){
-            throw new ValidationException(" Имя не может быть пустым");
+    public void validate(Checkout checkout){
+        if (checkout == null){
+            throw new ValidationException("Объект выдачи не может быть null");
+        }
+
+        if(checkout.getUsername() == null || checkout.getUsername().isBlank()){
+            throw new ValidationException("Имя не может быть пустым");
 
         }
-         if( checkout.getUsername().length() > 64){
-            throw new ValidationException(" Имя не может быть длинее 64 символов");
+        if(checkout.getUsername().length() > 64){
+            throw new ValidationException("Имя не может быть длиннее 64 символов");
+        }
 
-    }
-    if(checkout.getComment().length() > 128){
-throw  new  ValidationException("Кoмментарий не может быть длинее 128 символов");
-    }
-    if (checkout.getReturnCondition() == null){
-        throw new  ValidationException(" Состояние инструмента не может быть null");
-    }
+        if(checkout.getComment() != null && checkout.getComment().length() > 128){
+            throw  new  ValidationException("Кoмментарий не может быть длиннее 128 символов");
+        }
 
+        if (checkout.getOwnerUsername() == null || checkout.getOwnerUsername().isBlank()){
+            throw new ValidationException("Должно быть указано, кто оформлял выдачу");
+        }
+    }
 }
-}
-
