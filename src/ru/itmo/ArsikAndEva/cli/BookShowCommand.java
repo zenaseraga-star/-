b8513615d1cb2bb73ec098054ce583f30b1120ac
zenaseraga-star <zenaseraga@gmail.com>
2026-03-14 16:00:26 +0300

    Update BookShowCommand.java

--- src/ru/itmo/ArsikAndEva/cli/BookShowCommand.java
+++ src/ru/itmo/ArsikAndEva/cli/BookShowCommand.java
@@ -5,25 +5,23 @@ import ru.itmo.ArsikAndEva.exception.EntityNotFoundException;
 import  ru.itmo.ArsikAndEva.exception.ValidationException;
 import  ru.itmo.ArsikAndEva.manager.BookingManager;
 
-import java.util.Scanner;
 
 public class BookShowCommand implements Command {
     BookingManager bookingManager;
     Scanner scanner;
 
-    public BookShowCommand(BookingManager bookingManager, Scanner scanner) {
+    public BookShowCommand(BookingManager bookingManager) {
         this.bookingManager = bookingManager;
 
-        this.scanner = scanner;
     }
 
     @Override
     public void execute(String[] args) {
         try {
-            if (args.length < 1) {
+            if (args.length < 2) {
                 throw new ValidationException(" Введите Id ");
             }
-            long bookId = Long.parseLong(args[0]);
+            long bookId = Long.parseLong(args[1]);
 
             System.out.println(bookingManager.getBookById(bookId));
         } catch (NumberFormatException e) {
