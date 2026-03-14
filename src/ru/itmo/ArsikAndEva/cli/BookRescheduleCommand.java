5ea12c95d22839aa20c44bf027ed5170eabbc7d4
zenaseraga-star <zenaseraga@gmail.com>
2026-03-14 11:44:38 +0300

    Update BookRescheduleCommand.java

--- src/ru/itmo/ArsikAndEva/cli/BookRescheduleCommand.java
+++ src/ru/itmo/ArsikAndEva/cli/BookRescheduleCommand.java
@@ -1,4 +1,37 @@
 package ru.itmo.ArsikAndEva.cli;
 
-public class BookRescheduleCommand {
+
+import ru.itmo.ArsikAndEva.exception.EntityNotFoundException;
+import ru.itmo.ArsikAndEva.exception.ValidationException;
+import ru.itmo.ArsikAndEva.manager.BookingManager;
+import ru.itmo.ArsikAndEva.validator.BookingValidator;
+
+import java.util.Scanner;
+
+public class BookRescheduleCommand implements Command {
+    BookingManager bookingManager;
+    Scanner scanner;
+    public BookRescheduleCommand(BookingManager bookingManager, Scanner scanner){
+        this.bookingManager = bookingManager;
+        this.scanner = scanner;
+    }
+    @Override
+    public void execute(String[] args){
+        try{
+            if(args.length != 3){
+                throw new ValidationException( " Введите ID, время начала, время конца ");
+            }
+            long bookId = Long.parseLong(args[0]);
+            BookingValidator.validateTime(args[1]);
+            BookingValidator.validateTime(args[2]);
+            bookingManager.bookReschedule(bookId, args[1], args[2]);
+            System.out.println(" OK Бронь перенесена ");
+        }
+        catch  (NumberFormatException e) {
+            System.out.println(" ID должен быть числом ");
+        } catch (ValidationException | EntityNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
 }
