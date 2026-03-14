e8ae87c38c6e34a078883c81229cd237388cefbe
zenaseraga-star <zenaseraga@gmail.com>
2026-03-14 15:54:58 +0300

    Update BookListCommand.java

--- src/ru/itmo/ArsikAndEva/cli/BookListCommand.java
+++ src/ru/itmo/ArsikAndEva/cli/BookListCommand.java
@@ -9,14 +9,11 @@ import ru.itmo.ArsikAndEva.model.Booking;
 import ru.itmo.ArsikAndEva.validator.BookingValidator;
 
 import java.util.List;
-import java.util.Scanner;
 
 public class BookListCommand implements Command {
     BookingManager bookingManager;
-    Scanner scanner;
- public   BookListCommand(BookingManager bookingManager, Scanner scanner){
+ public   BookListCommand(BookingManager bookingManager){
      this.bookingManager = bookingManager;
-     this.scanner = scanner;
  }
  @Override
     public void execute(String[] args){
