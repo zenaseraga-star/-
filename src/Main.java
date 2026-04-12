cbf283a8421122af41690287bbd0fd3d11fe8d04
Aln0270 <arslan.uzenbayev.07@bk.ru>
2026-04-12 12:07:17 +0300

    init: setup project structure and gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import ru.itmo.ArsikAndEva.cli.ConsoleApp;
+
+public class Main {
+    public static void main(String[] args){
+        ConsoleApp consoleApp = new ConsoleApp();
+        consoleApp.start();
+    }
+}
+    
---
+++ src/ru/itmo/ArsikAndEva/model/enums/InstrumentStatus.java
@@ -0,0 +1,6 @@
+package ru.itmo.ArsikAndEva.model.enums;
+
+public enum InstrumentStatus {
+    ACTIVE,
+    OUT_OF_SERVICE
+}
